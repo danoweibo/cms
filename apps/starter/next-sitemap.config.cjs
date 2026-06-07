c361508e12467b937f15910f57b2a28b0f32bb47
Daniel Oweibo <112868914+danoweibo@users.noreply.github.com>
2026-06-07 22:00:30 +0100

    Update next-sitemap.config.cjs

--- apps/starter/next-sitemap.config.cjs
+++ apps/starter/next-sitemap.config.cjs
@@ -1,7 +1,7 @@
 const SITE_URL =
   process.env.NEXT_PUBLIC_SERVER_URL ||
   process.env.VERCEL_PROJECT_PRODUCTION_URL ||
-  'https://example.com'
+  'https://cms.dvnlcorp.com'
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
