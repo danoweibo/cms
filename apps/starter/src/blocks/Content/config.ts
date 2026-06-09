5969675c838c62a196385d8ab080327f094a3c44
Daniel Oweibo <112868914+danoweibo@users.noreply.github.com>
2026-06-09 23:44:06 +0100

    update component blocks

--- apps/starter/src/blocks/Content/config.ts
+++ apps/starter/src/blocks/Content/config.ts
@@ -1,46 +1,46 @@
-import type { Block, Field } from 'payload'
+import type { Block, Field } from "payload"
 
 import {
   FixedToolbarFeature,
   HeadingFeature,
   InlineToolbarFeature,
   lexicalEditor,
-} from '@payloadcms/richtext-lexical'
+} from "@payloadcms/richtext-lexical"
 
-import { link } from '@/fields/link'
+import { link } from "@/fields/link"
 
 const columnFields: Field[] = [
   {
-    name: 'size',
-    type: 'select',
-    defaultValue: 'oneThird',
+    name: "size",
+    type: "select",
+    defaultValue: "oneThird",
     options: [
       {
-        label: 'One Third',
-        value: 'oneThird',
+        label: "One Third",
+        value: "oneThird",
       },
       {
-        label: 'Half',
-        value: 'half',
+        label: "Half",
+        value: "half",
       },
       {
-        label: 'Two Thirds',
-        value: 'twoThirds',
+        label: "Two Thirds",
+        value: "twoThirds",
       },
       {
-        label: 'Full',
-        value: 'full',
+        label: "Full",
+        value: "full",
       },
     ],
   },
   {
-    name: 'richText',
-    type: 'richText',
+    name: "richText",
+    type: "richText",
     editor: lexicalEditor({
       features: ({ rootFeatures }) => {
         return [
           ...rootFeatures,
-          HeadingFeature({ enabledHeadingSizes: ['h2', 'h3', 'h4'] }),
+          HeadingFeature({ enabledHeadingSizes: ["h2", "h3", "h4"] }),
           FixedToolbarFeature(),
           InlineToolbarFeature(),
         ]
@@ -49,8 +49,8 @@ const columnFields: Field[] = [
     label: false,
   },
   {
-    name: 'enableLink',
-    type: 'checkbox',
+    name: "enableLink",
+    type: "checkbox",
   },
   link({
     overrides: {
@@ -64,12 +64,12 @@ const columnFields: Field[] = [
 ]
 
 export const Content: Block = {
-  slug: 'content',
-  interfaceName: 'ContentBlock',
+  slug: "content",
+  interfaceName: "ContentBlock",
   fields: [
     {
-      name: 'columns',
-      type: 'array',
+      name: "columns",
+      type: "array",
       admin: {
         initCollapsed: true,
       },
