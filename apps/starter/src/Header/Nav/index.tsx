c3664a26a39d0790fadc8d94e649884b76dbeeea
Daniel Oweibo <112868914+danoweibo@users.noreply.github.com>
2026-06-12 23:24:04 +0100

    update payload components

--- apps/starter/src/Header/Nav/index.tsx
+++ apps/starter/src/Header/Nav/index.tsx
@@ -1,12 +1,12 @@
-'use client'
+"use client"
 
-import React from 'react'
+import React from "react"
 
-import type { Header as HeaderType } from '@/payload-types'
+import type { Header as HeaderType } from "@/payload-types"
 
-import { CMSLink } from '@/components/Link'
-import Link from 'next/link'
-import { SearchIcon } from 'lucide-react'
+import { CMSLink } from "@/components/Link"
+import Link from "next/link"
+import { SearchIcon } from "lucide-react"
 
 export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
   const navItems = data?.navItems || []
