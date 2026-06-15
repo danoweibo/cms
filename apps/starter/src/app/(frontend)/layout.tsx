dd31549d369134357e3cf62c734a4708796bd489
Daniel Oweibo <112868914+danoweibo@users.noreply.github.com>
2026-06-16 00:47:46 +0100

    update payload components

--- apps/starter/src/app/(frontend)/layout.tsx
+++ apps/starter/src/app/(frontend)/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from 'next'
+import type { Metadata } from "next"
 
-import { cn } from '@/utilities/ui'
-import { GeistMono } from 'geist/font/mono'
-import { GeistSans } from 'geist/font/sans'
-import React from 'react'
+import { cn } from "@/utilities/ui"
+import { GeistMono } from "geist/font/mono"
+import { GeistSans } from "geist/font/sans"
+import React from "react"
 
-import { AdminBar } from '@/components/AdminBar'
-import { Footer } from '@/Footer/Component'
-import { Header } from '@/Header/Component'
-import { Providers } from '@/providers'
-import { InitTheme } from '@/providers/Theme/InitTheme'
-import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
-import { draftMode } from 'next/headers'
+import { AdminBar } from "@/components/AdminBar"
+import { Footer } from "@/Footer/Component"
+import { Header } from "@/Header/Component"
+import { Providers } from "@/providers"
+import { InitTheme } from "@/providers/Theme/InitTheme"
+import { mergeOpenGraph } from "@/utilities/mergeOpenGraph"
+import { draftMode } from "next/headers"
 
-import './globals.css'
-import { getServerSideURL } from '@/utilities/getURL'
+import "./globals.css"
+import { getServerSideURL } from "@/utilities/getURL"
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const { isEnabled } = await draftMode()
@@ -47,7 +47,7 @@ export const metadata: Metadata = {
   metadataBase: new URL(getServerSideURL()),
   openGraph: mergeOpenGraph(),
   twitter: {
-    card: 'summary_large_image',
-    creator: '@payloadcms',
+    card: "summary_large_image",
+    creator: "@payloadcms",
   },
 }
